155c3b3ab87f1b29c6e500f704e93ffaa8c6cc61
Shahjahan M.Aslam <shahjahanmuhammadaslam@gmail.com>
2022-11-10 13:04:38 +0500

    assignment 3

---
+++ app.js
@@ -0,0 +1,51 @@
+var city0 = "Pakistan"
+var city1 = "India"
+var city2 = "China"
+var city3 = "Brazil"
+var city4 = "Africa"
+var city5 = "Iran"
+
+alert ("welcome to " + city3)
+
+var animals = ["Cat", "Dog", "Lion", "Monkey", "Tiger"]
+console.log(animals)
+console.log(animals [3])
+
+
+var a = ["ate", "bat", "cat", "dog", "egg"]
+a.push ("fin")
+console.log(a)
+
+var b = ["gun", "hen", "ice", "jug"]
+b.pop ()
+console.log(b)
+
+var c = ["kit", "let", "met",]
+c.shift ()
+console.log(c)
+
+var d = ["not", "off", "pet"]
+d.unshift ("why")
+console.log(d)
+
+var e = ["queen", "red", "sad", "tent"]
+var f = e.slice (1, 3)
+console.log(f)
+
+var g = ["umbrella", "van", "what", "x-ray", "yet", "zee"]
+g.splice (3, 2)
+console.log(g)
+
+var h = ["a", "b", "c", "d", "e", "f", "g"]
+var h = h.indexOf ("e")
+console.log(h)
+
+
+var net = ["a", "b", "c", "d", "e", "f", "g", "a", "b", "c", "d", "e", "f", "g"]
+var x = net.length
+console.log(x)
+
+
+for (var k = 0; k < 10; k++) {
+    console.log("7 X " + (k + 1) + " = " + 7 * (k + 1))
+}
